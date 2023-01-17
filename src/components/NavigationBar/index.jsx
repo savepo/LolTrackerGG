7d359bca803fa3df4faa49e1d26de81eebf60d5c
anderson <andersonbarreto2001@gmail.com>
2023-01-17 13:34:57 +0100

    main container of the components GameName and regions

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,14 +1,33 @@
-import { GetSumoner } from '../../helpers/api.helper'
-const testApi = () => {
-  return GetSumoner('euw1', 'DinoKlk')
-}
+import React from 'react'
+import DropdownSelect from '../DropdownSelect'
+import InputGameName from '../InputGameName'
+import { NavigationBarContainer } from './styles'
 
-const pintaDatos = () => {
+const NavigationBar = () => {
+  const regions = [
+    { value: 'BR1', label: 'Brazil' },
+    { value: 'EUN1', label: 'Europe Nordic & East' },
+    { value: 'EUW1', label: 'Europe West' },
+    { value: 'JP1', label: 'Japan' },
+    { value: 'KR', label: 'Korea' },
+    { value: 'LA1', label: 'Latin America North' },
+    { value: 'LA2', label: 'Latin America South' },
+    { value: 'NA1', label: 'North America' },
+    { value: 'OC1', label: 'Oceania' },
+    { value: 'PH2', label: 'The Philippines' },
+    { value: 'RU1', label: 'Russia' },
+    { value: 'SG2', label: 'Singapore, Malaysia & Indonesia' },
+    { value: 'TH2', label: 'Thailand' },
+    { value: 'TR1', label: 'Turkey' },
+    { value: 'TW2', label: 'Taiwan, Hong Kong and Macao' },
+    { value: 'VN2', label: 'Vietnam' }
+  ]
   return (
-    <div>
-      {testApi()}
-    </div>
+    <NavigationBarContainer>
+      <DropdownSelect placeholder={regions[2].label} options={regions} />
+      <InputGameName />
+    </NavigationBarContainer>
   )
 }
 
-export default pintaDatos
+export default NavigationBar
---
+++ src/components/NavigationBar/styles.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const NavigationBarContainer = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+border-radius: 1.863rem 1.863rem 1.863rem 1.863rem;
+border: 1px solid #ccc;
+width: 39rem;
+`
