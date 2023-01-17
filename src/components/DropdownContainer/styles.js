821f141ad31044957a5499883a461ce321afde58
anderson <andersonbarreto2001@gmail.com>
2023-01-17 13:35:24 +0100

    deleting folder

--- src/components/DropdownContainer/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled'
-import { withTheme } from '../../helpers/styles.helper'
-
-export const DropdownContainer = styled.div`
- text-align: left;
- position: relative;
- border: 1px solid #ccc;
- ${withTheme('border-radius', 'sizes.rem03')}
-`
